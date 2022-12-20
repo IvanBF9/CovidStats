ad1a0be8b0177e4ba6c791e564ab9797bbb43fc9
IvanBF9 <ivanbraga@outlook.fr>
2022-12-20 16:13:47 +0100

    update

--- main.R
+++ main.R
@@ -1,6 +1,9 @@
 library(dplyr)
-data <- read.csv(file = "vacsi-s-a-reg-2022-12-19-19h00.csv")
+data <- read.csv(file = "vacsi-s-a-reg-2022-12-19-19h00.csv",sep = ";", header =TRUE)
 
-View(data)
+# data <- na.omit(data)
+# data <- data[!apply(data[-1:-3], 1, function(x) all(x == 0)), ]
+names(data)
+View(data[1])
 
-mean(data$clage_vacsi)
+#mean(data$clage_vacsi)
