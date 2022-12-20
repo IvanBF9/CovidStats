f282c03df65d6a00993c85049b48a0862929b13a
IvanBF9 <ivanbraga@outlook.fr>
2022-12-20 16:17:29 +0100

    update git push

--- main.R
+++ main.R
@@ -1,9 +1,10 @@
 library(dplyr)
 data <- read.csv(file = "vacsi-s-a-reg-2022-12-19-19h00.csv",sep = ";", header =TRUE)
 
-# data <- na.omit(data)
-# data <- data[!apply(data[-1:-3], 1, function(x) all(x == 0)), ]
-names(data)
-View(data[1])
 
-#mean(data$clage_vacsi)
+data <- na.omit(data)
+
+names(data)
+View(data)
+
+mean(data$clage_vacsi)
