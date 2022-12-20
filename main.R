8d363a60c201d07bd8390fbc1b563199d22eafda
IvanBF9 <ivanbraga@outlook.fr>
2022-12-20 16:42:14 +0100

    update

--- main.R
+++ main.R
@@ -1,9 +1,12 @@
 library(dplyr)
-data <- read.csv(file = "vacsi-s-a-reg-2022-12-19-19h00.csv",sep = ";", header =TRUE)
 
+# Read csv
+data <- read.csv(file = "vacsi-s-a-reg-2022-12-19-19h00.csv", sep=";", header=TRUE)
 
+# Drop na
 data <- na.omit(data)
 
+# get name of columns and display df
 names(data)
 View(data)
 
