6fa08d81558fa4218b9ded6e5ef672a5de74839c
LouisPLN <louispoulinpro@gmail.com>
2022-12-23 09:20:56 +0100

    shiny

--- shiny.R
+++ shiny.R
@@ -4,26 +4,25 @@ library(shinythemes)
 source("./main.R")
 
 # Définition de l'interface utilisateur de notre application 
-ui <- shinyUI(fluidPage(theme = shinytheme("cyborg"),
+ui <- shinyUI(fluidPage(theme = shinytheme("slate"),
   
   # Le titre de votre application
   fluidRow(
-      column(10, align="center", offset = 2,
+        width = 12,
       tags$h1("Statistiques sur le COVID-19"), 
       tags$hr(), 
       tags$style(type="text/css", "h1{text-align:center; font-size: 40px; font-weight: bold;}"),
       tags$style(type="text/css", "hr{padding: 20px;}"),
-      )
    ),
 
   sidebarLayout(
-        sidebarPanel(
+        sidebarPanel(width = 6,
+            tags$h3("Statistiques sur le COVID-19"),
+            plotOutput("vaccinPlot")
         ),
-        mainPanel(
-            column(6, align="center", offset = 3, 
-                plotOutput("vaccinPlot"),
-            ),
-        ),
+        mainPanel(width = 6,
+            tags$h3("Statistiques sur le COVID-19")
+        )
     ),
 ))
 
